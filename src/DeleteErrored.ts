c7c7022bc58a1a652736ba159941c0a1e3864afd
Gabriel Porto <gabriel_porto@outlook.com>
2023-04-15 21:46:23 -0300

    feat: util delete error files

---
+++ src/DeleteErrored.ts
@@ -0,0 +1,31 @@
+import chalk from "chalk";
+import erroredFile from "../data/errored.json";
+
+/**
+ * This is a simple script to delete all the errored videos from BunnyCDN
+ * It's not a part of the migration process, but it's useful to have around
+ * in case you need to delete a bunch of videos from BunnyCDN.
+ */
+
+async function deleteErrored() {
+  for (const errored of erroredFile as { bunnyId: string }[]) {
+    const url = `https://video.bunnycdn.com/library/${process.env.BUNNY_LIBRARY_ID}/videos/${errored.bunnyId}`;
+    const options = {
+      method: "DELETE",
+      headers: {
+        accept: "application/json",
+        AccessKey: process.env.BUNNY_ACCESS_KEY ?? "",
+      },
+    };
+
+    const resp = await fetch(url, options)
+      .then((res) => res.json())
+      .catch((err) => {
+        console.log(chalk.red(`Error deleting ${errored.bunnyId}`));
+      });
+
+    console.log(resp);
+  }
+}
+
+deleteErrored();
