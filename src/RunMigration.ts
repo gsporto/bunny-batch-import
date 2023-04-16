38cae5ad387fa93ccc836b9fa7c7d452d26d76c6
Gabriel Porto <gabriel_porto@outlook.com>
2023-04-15 21:41:08 -0300

    feat: change use case to bunny

--- src/RunMigration.ts
+++ src/RunMigration.ts
@@ -1,21 +1,72 @@
-require('dotenv/config');
+require("dotenv/config");
 
-import chalk from 'chalk';
-import { queue } from './lib/queue';
+import chalk from "chalk";
+import { queue } from "./lib/queue";
 
-import videos from '../data/videos.json';
+import videosFile from "../data/videos.json";
+import uploadedFile from "../data/uploaded.json";
+import erroredFile from "../data/errored.json";
+import { writeFile } from "fs/promises";
+import path from "path";
+
+const uploaded = uploadedFile as { muxId: string; bunnyId: string }[];
+const errored = erroredFile as { muxId: string; bunnyId: string }[];
+const videos = videosFile as string[];
+
+async function logEverything() {
+  await writeFile(
+    path.resolve(__dirname, "../data/uploaded.json"),
+    JSON.stringify(uploaded, null, 2)
+  );
+  await writeFile(
+    path.resolve(__dirname, "../data/errored.json"),
+    JSON.stringify(errored, null, 2)
+  );
+  await writeFile(
+    path.resolve(__dirname, "../data/videos.json"),
+    JSON.stringify(videos, null, 2)
+  );
+  return;
+}
 
 async function populateQueue() {
-  Object.entries(videos).forEach(async ([id, downloadUrl]) => {
+  videos.forEach(async (id) => {
     queue
-      .push({ videoId: id, url: downloadUrl })
-      .then((uploadId) => {
+      .push({
+        videoId: id,
+        url: `https://stream.mux.com/${id}/high.mp4?download=live-high`,
+      })
+      .then(async ({ uploadId, videoId }) => {
+        uploaded.push({
+          muxId: videoId,
+          bunnyId: uploadId,
+        });
+
+        videos.splice(videos.indexOf(id), 1);
+
+        await logEverything();
+
         console.log(chalk.green(`Uploaded: ${id} (Upload ID: ${uploadId})`));
       })
-      .catch((err) => {
-        console.log(chalk.red(`Error on video: ${id}`), err);
+      .catch(async ({ uploadId, videoId }) => {
+        errored.push({
+          muxId: videoId,
+          bunnyId: uploadId,
+        });
+
+        await logEverything();
+
+        console.log(chalk.red(`Error on video: ${id}`), {
+          muxId: videoId,
+          bunnyId: uploadId,
+        });
       });
-  })
-};
+  });
+}
 
 populateQueue();
+
+queue.drained().then(async () => {
+  await logEverything();
+  console.log(chalk.green("Queue drained!"));
+});
--- src/lib/worker.ts
+++ src/lib/worker.ts
@@ -1,26 +1,50 @@
 import request from "request";
-import { mux } from "../services/mux";
 import { QueueTask } from "./queue";
 
+const options = {
+  method: "POST",
+  headers: {
+    accept: "application/json",
+    "content-type": "application/*+json",
+    AccessKey: process.env.BUNNY_ACCESS_KEY ?? "",
+  },
+};
+
 export async function worker({ videoId, url }: QueueTask) {
-  const upload = await mux.Video.Uploads.create({
-    cors_origin: '*', 
-    new_asset_settings: {
-      passthrough: videoId,
-      playback_policy: 'public',
+  const upload = await fetch(
+    `https://video.bunnycdn.com/library/${process.env.BUNNY_LIBRARY_ID}/videos`,
+    {
+      ...options,
+      body: JSON.stringify({
+        title: `imported-${videoId}`,
+      }),
     }
-  })
+  ).then((res) => res.json());
 
-  const uploadId = upload.id;
+  const uploadId = upload.guid;
 
-  return await new Promise<string>((resolve, reject) => {
-    request(url, { followAllRedirects: true })
-      .on('complete', () => { 
-        resolve(uploadId);
-      })
-      .on('error', (err) => { 
-        reject(err);
-      })  
-      .pipe(request.put(upload.url))
-  })
+  return await new Promise<{ uploadId: string; videoId: string }>(
+    (resolve, reject) => {
+      request(url)
+        .on("complete", (resp) => {
+          if (resp.toJSON().statusCode !== 200) {
+            reject({ uploadId, videoId });
+          }
+          resolve({ uploadId, videoId });
+        })
+        .on("error", (err) => {
+          reject({ uploadId, videoId });
+        })
+        .pipe(
+          request.put(
+            `https://video.bunnycdn.com/library/${process.env.BUNNY_LIBRARY_ID}/videos/${uploadId}`,
+            {
+              headers: {
+                AccessKey: process.env.BUNNY_ACCESS_KEY ?? "",
+              },
+            }
+          )
+        );
+    }
+  );
 }
